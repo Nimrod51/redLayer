db3d84d95e272a0858db1837619fd798e392739b
Nimrod <ngavish@protonmail.com>
2025-12-04 16:16:43 +0100

    Update redLayerModule.py
    Popupmode is required
    
    Co-authored-by: Hannes <kannes@users.noreply.github.com>

--- redLayerModule.py
+++ redLayerModule.py
@@ -305,6 +305,7 @@ class redLayer(QgsMapTool):
         canvas_toolbutton = QToolButton(self.iface.mainWindow())
         canvas_toolbutton.setDefaultAction(self.canvasButton)
         canvas_toolbutton.setMenu(self.canvasMenu())
+        canvas_toolbutton.setPopupMode(QToolButton.InstantPopup)
         self.toolbar.addWidget(canvas_toolbutton)
         self.canvasToolButton = canvas_toolbutton
         self.noteButton.setCheckable(True)
